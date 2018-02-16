ba8b545e7eb4df9b831bba7f091c64b22c567f63
FStriewski <fstriewski@outlook.com>
2018-02-16 12:13:16 +0100

    Use index and show methods

--- tacta.rb
+++ tacta.rb
@@ -1,29 +1,33 @@
 contacts = []
 
-contacts << { name: "Thomas Jefferson", phone: "+1 206 310 1369" , email: "tjeff@us.gov" }
-contacts << { name: "Charles Darwin"  , phone: "+44 20 7123 4567", email: "darles@evolve.org" }
-contacts << { name: "Nikola Tesla"    , phone: "+385 43 987 3355", email: "nik@inductlabs.com" }
-contacts << { name: "Genghis Khan"    , phone: "+976 2 194 2222" , email: "contact@empire.com" }
-contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@theroost.org" }
+  contacts << { name: "Thomas Jefferson", phone: "+1 206 310 1369" , email: "tjeff@us.gov" }
+  contacts << { name: "Charles Darwin"  , phone: "+44 20 7123 4567", email: "darles@evolve.org" }
+  contacts << { name: "Nikola Tesla"    , phone: "+385 43 987 3355", email: "nik@inductlabs.com" }
+  contacts << { name: "Genghis Khan"    , phone: "+976 2 194 2222" , email: "contact@empire.com" }
+  contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@theroost.org" }
 
-
-def index(contacts)
-  contacts.each_with_index do |contact, index|
-    puts "#{index+1}) #{contact[:name]}"
+  def index(contacts)
+    contacts.each_with_index do |contact, index|
+      puts "#{index+1}) #{contact[:name]}"
+    end
   end
-end
 
-puts
-print "Who would you like to see? "
-response = gets.chomp
+  def show(contact)
+    puts
+    puts contact[:name].to_s
+    puts "phone: #{contact[:phone]}"
+    puts "email: #{contact[:email]}"
+  end
 
-id = response.to_i
+index(contacts)
 
-contact = contacts[id - 1]
-
-def show(contact)
   puts
-  puts contact[:name].to_s
-  puts "phone: #{contact[:phone]}"
-  puts "email: #{contact[:email]}"
-end
+  print "Who would you like to see? "
+  response = gets.chomp
+
+  id = response.to_i
+
+  contact = contacts[id - 1]
+
+  puts
+  show(contact)
